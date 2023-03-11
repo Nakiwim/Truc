b198c6a601e3bda9242de14c98b556c293fe8006
Nakiwim <nakim.halidi@outlook.com>
2023-03-11 21:14:47 +0100

    test

---
+++ Jsp.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+
+int main() {
+    int coef_colonne = 0;
+    int coef_ligne = 0;
+    int matricea[0][0];
+    int matriceb[0][0];
+    int nb_colonne = 0;
+    int nb_ligne = 0;
+    int i = 0;
+    int j = 0;
+
+    cout << "choix du nombre de ligne : " << endl;
+    cin >> nb_ligne;
+
+    cout << "choix du nombre de colonne : " << endl;
+    cin >> nb_colonne;
+
+    for(i = 0; i < nb_ligne; i++)
+        for(j = 0; j < nb_colonne; j++)
+        {
+            cout << "Coefficient de la matrice a la ligne \n L = " << i << " ------ Colonne C = " << j << endl;
+            cin >> matricea[i][j];
+            cout << "A" << i + 1 << j + 1 << " = " << matricea[i][j] << endl;
+
+        }
+
+    for(i = 0; i < nb_ligne; i++)
+        for(j = 0; j < nb_colonne; j++)
+        {
+            cout << "Coefficient de la matrice a la ligne \n L = " << i << " ------ Colonne C = " << j << endl;
+            cin >> matriceb[i][j];
+            cout << "B" << i + 1 << j + 1 << " = " << matriceb[i][j] << endl;
+
+        }
+
+    return 0;
+}
